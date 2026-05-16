7e8985cf9adb6c83779e2519925905e0da6de6b8
Lucas-Siqueira-Dev <lucas.limasiqueira2016@gmail.com>
2026-05-16 18:36:46 -0300

    build: adiciona dockerfile para deploy da API no Render

--- FinScan.API/Program.cs
+++ FinScan.API/Program.cs
@@ -11,13 +11,25 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
+    app.UseCors("AllowAll");
+    app.UseAuthorization();
 
 app.MapControllers();
 app.Run();

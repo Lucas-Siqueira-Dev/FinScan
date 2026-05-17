3c471b4cc4cff639779f692cb1a9e6844335c241
Guilherme <guilhermeaferreira1234@gmail.com>
2026-05-17 18:15:06 -0300

    feat: cria tela de simulacao de investimentos (FSVS-33)

---
+++ FinScan.App/Views/SimulacaoPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using FinScan.App.ViewModels;
+
+namespace FinScan.App.Views;
+
+public partial class SimulacaoPage : ContentPage
+{
+    public SimulacaoPage(SimulacaoViewModel viewModel)
+    {
+        InitializeComponent();
+        BindingContext = viewModel;
+    }
+}

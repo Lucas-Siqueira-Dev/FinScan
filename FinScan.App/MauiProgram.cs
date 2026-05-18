9204e9664774dd952925f664b3c23a17910500aa
Lucas-Siqueira-Dev <lucas.limasiqueira2016@gmail.com>
2026-05-17 21:29:35 -0300

    fix: configura injecao de dependencia, atualiza sdk e inicializa microcharts para resolver crash no boot

--- FinScan.App/MauiProgram.cs
+++ FinScan.App/MauiProgram.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.Extensions.Logging;
+using FinScan.App.Services;
+using FinScan.App.ViewModels;
+using FinScan.App.Views;
+using Microcharts.Maui;
 
 namespace FinScan.App;
 
@@ -9,11 +13,25 @@ public static class MauiProgram
         var builder = MauiApp.CreateBuilder();
         builder
             .UseMauiApp<App>()
+            .UseMicrocharts()
             .ConfigureFonts(fonts =>
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             });
+
+        // 1. Registra os Serviços (Regra de Negócio e APIs)
+        // AddSingleton garante que teremos apenas um HttpClient rodando no app inteiro
+        builder.Services.AddSingleton<ApiService>();
+
+        // 2. Registra as ViewModels (A Lógica das Telas)
+        // AddTransient cria uma nova ViewModel sempre que a tela for aberta
+        builder.Services.AddTransient<SimulacaoViewModel>();
+
+        // 3. Registra as Páginas (As Telas Físicas)
+        builder.Services.AddTransient<MainPage>(); 
+        
+        builder.Services.AddTransient<SimulacaoPage>(); 
 
 #if DEBUG
         builder.Logging.AddDebug();
